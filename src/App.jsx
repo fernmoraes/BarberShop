189e6266f252164cabc8d19da66f3193f7f9bf8c
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-10-22 20:22:42 -0300

    Barber Shop projeto iniciado

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+import Footer from './components/Footer/Footer'
+import Nav from './components/Nav/Nav'
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <h6>@-2024 Todos os direitos reservados</h6>
+    </div>
+  )
+}
